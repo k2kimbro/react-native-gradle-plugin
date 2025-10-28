3b90af767d0284010fe71a595ca056d3339a60db
Ken Kimbrough <kkimbrou@KIMBROUGH.NET>
2025-10-28 13:50:17 -0400

    Add ReactRootProjectPlugin and sourceSets config

---
+++ src/main/kotlin/com/facebook/react/ReactRootProjectPlugin.kt
@@ -0,0 +1,10 @@
+package com.facebook.react
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ReactRootProjectPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        // Plugin logic here (can be empty for now)
+    }
+}
